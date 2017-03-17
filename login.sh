3b394ee781267d081b32a65bbce460223ed2c792
fireapp-Admin <blair1226@gmail.com>
2017-03-17 16:15:27 +0800

    add --drop to mongorestore to drop collection before restore.

--- login.sh
+++ login.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-gcloud auth activate-service-account --key-file $1
+if [ -z "$1" ];
+then gcloud auth activate-service-account --key-file /ken/gcloud-storage-key.json
+else gcloud auth activate-service-account --key-file $1
+fi
--- restore.sh
+++ restore.sh
@@ -2,5 +2,5 @@
 
 DATE=$(date +"%m_%d_%Y")
 gsutil -m cp -r gs://firecommand_db/$DATE .
-mongorestore -d firecommand_pro -h 127.0.0.1 $DATE/firecommand_pro
+mongorestore --drop -d firecommand_pro -h 127.0.0.1 $DATE/firecommand_pro
 rm -rf $DATE
