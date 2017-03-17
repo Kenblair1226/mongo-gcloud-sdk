eb54ce0aa6448154964186b8853f4481a02e6122
fireapp-Admin <blair1226@gmail.com>
2017-03-17 16:50:42 +0800

    Fix a typo

--- login.sh
+++ login.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 if [ -z "$1" ];
-then gcloud auth activate-service-account --key-file /ken/gcloud-storage-key.json
+then gcloud auth activate-service-account --key-file /key/gcloud-storage-key.json
 else gcloud auth activate-service-account --key-file $1
 fi
