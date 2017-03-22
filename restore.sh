cd31ffef227a693f9d4910dd18249ed3b4de30be
Chien Chih Chen <blair1226@gmail.com>
2017-03-22 17:26:45 +0800

    Fix script bugs.

--- restore.sh
+++ restore.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 if [ -z "$1" ];
-then DATE=$1
-else DATE=$(date +"%m_%d_%Y")
+then DATE=$(date +"%m_%d_%Y")
+else DATE=$1
 fi
 gsutil -m cp -r gs://firecommand_db/$DATE .
 rm $DATE/firecommand_pro/accesstokens.* $DATE/firecommand_pro/refreshtokens.*
-mongorestore --drop -d firecommand_pro -h 127.0.0.1 $DATE/firecommand_pro
+mongorestore --drop -d firecommand_pro -h mongo $DATE/firecommand_pro
 rm -rf $DATE
