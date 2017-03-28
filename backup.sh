25199bec582eac4497a60dc5cf2b5a5a5b43f12a
Chien Chih Chen <blair1226@gmail.com>
2017-03-28 15:35:35 +0800

    update scripts

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Preparing backup..." >>/var/log/cron.log 2>&1
+echo "Preparing backup..."
 DATE=$(date +"%m_%d_%Y")
 mkdir $DATE
-echo "Backup database..." >>/var/log/cron.log 2>&1
+echo "Backup database..."
 mongodump -d firecommand_pro -o $DATE -h mongo
-echo "Uploading database..." >>/var/log/cron.log 2>&1
-gsutil -m cp -r $DATE gs://firecommand_db/
+echo "Uploading database..."
+/google-cloud-sdk/bin/gsutil -m cp -r $DATE gs://firecommand_db/
 rm -rf $DATE
-echo "Done backup!" >>/var/log/cron.log 2>&1
+echo "Done backup!"
--- restore.sh
+++ restore.sh
@@ -4,10 +4,10 @@ if [ -z "$1" ];
 then DATE=$(date +"%m_%d_%Y")
 else DATE=$1
 fi
-echo "Download database... $DATE" >>/var/log/cron.log 2>&1
+echo "Download database... $DATE"
 gsutil -m cp -r gs://firecommand_db/$DATE .
 rm $DATE/firecommand_pro/accesstokens.* $DATE/firecommand_pro/refreshtokens.*
-echo "Restoring database..." >>/var/log/cron.log 2>&1
+echo "Restoring database..."
 mongorestore --drop -d firecommand_pro -h mongo $DATE/firecommand_pro
 rm -rf $DATE
-echo "Done restoring!" >>/var/log/cron.log 2>&1
+echo "Done restoring!"
