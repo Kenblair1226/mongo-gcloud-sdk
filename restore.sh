1f5058b22c03ed749f39460dae5173dbb9f89b9f
Chien Chih Chen <blair1226@gmail.com>
2017-03-28 15:49:33 +0800

    update scripts

--- restore.sh
+++ restore.sh
@@ -5,7 +5,7 @@ then DATE=$(date +"%m_%d_%Y")
 else DATE=$1
 fi
 echo "Download database... $DATE"
-gsutil -m cp -r gs://firecommand_db/$DATE .
+/google-cloud-sdk/gsutil -m cp -r gs://firecommand_db/$DATE .
 rm $DATE/firecommand_pro/accesstokens.* $DATE/firecommand_pro/refreshtokens.*
 echo "Restoring database..."
 mongorestore --drop -d firecommand_pro -h mongo $DATE/firecommand_pro
